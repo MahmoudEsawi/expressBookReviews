283c569042c13539f1faf6f1c5d04b3f6b8b7231
Mahmoud Esawi <esawi@ieee.org>
2026-05-27 01:45:57 +0300

    Final project: all routes implemented with async/await versions

--- final_project/index.js
+++ final_project/index.js
@@ -33,6 +33,6 @@ app.use("/customer/auth/*", function auth(req, res, next) {
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-const PORT = 5000;
+const PORT = 5001;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
